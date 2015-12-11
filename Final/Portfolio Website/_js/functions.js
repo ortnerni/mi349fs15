c4a0d05742c9aa52bea3a16bcf98fb02feedd5a4
ortnerni <ortnerni@msu.edu>
2015-12-11 18:34:12 -0500

    Final
    DONE ^.^

---
+++ Final/Portfolio Website/_js/functions.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+<!--
+test = new Date()
+month = test.getMonth()
+month = (month * 1) + 1
+day = test.getDate()
+year = test.getFullYear()
+document.write(" ",month,"/",day,"/",year," ")
+-->
+
+
